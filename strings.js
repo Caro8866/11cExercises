7e779f27d0ac949409e78d127bc7530161b113b8
Caroline Thostrup <caro342h@stud.kea.dk>
2022-08-26 13:31:10 +0200

    string notes

---
+++ strings.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// *Gives a santa claus emoji*
+// const unicode = "\u{1f385}";
+// console.log(unicode);
+
+// *line breaking*
+
+// const longLine = "A very, very, very, very, VERY! long line of text, \nthat spans across the width of the editor!";
+// console.log(longLine);
+
+// *Template literals*
+// const name = "Caroline";
+// const drink = "Fake Kondi";
+
+// > can include auto line break
+// console.log(`Hello ${name},
+// would you like a ${drink}?`);
+
+// > classic concatenation
+// console.log("Hello " + name + ", would you like a " + drink + "?");

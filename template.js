9788fe0030fec990afea482d650df8b3dd2f2426
Caroline Thostrup <caro342h@stud.kea.dk>
2022-08-26 13:39:31 +0200

    Template literals done

---
+++ template.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// *Template literal Ex*
+const name = "Caroline";
+const animalType = "dog";
+const animalName = "Bella";
+
+console.log(`My name is ${name}. \nI have a ${animalType} called ${animalName}.`);

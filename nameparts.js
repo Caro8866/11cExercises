9ea84ecca272c9c715c8f2db99867f601089e8cc
Caroline Thostrup <caro342h@stud.kea.dk>
2022-08-30 12:21:42 +0200

    added folder for outlines

---
+++ nameparts.js
@@ -0,0 +1,15 @@
+"use strict";
+const fullName = "Caroline ALbinusBjarnø Thostrup";
+
+// First name
+let firstName = fullName.substring(0, 8);
+console.log(`My first name is ${firstName}`);
+
+// Middle name
+let middleName = fullName.substring(9, 22);
+console.log(`My middle name is ${middleName}`);
+
+// Lasr name
+/* let middleName = fullName.substring(9, 22);
+console.log(`My middle name is ${middleName}`);
+ */

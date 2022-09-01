49734f24c93972d166a4698e0b195217f0fcf516
Caroline Thostrup <caro342h@stud.kea.dk>
2022-09-01 21:22:18 +0200

    first steps

--- objects/objects.js
+++ objects/objects.js
@@ -1,9 +1,44 @@
 // *Adding a property
 
-let person1 = {
+const person1 = {
   firstName: "Caroline",
   age: 22,
   student: true,
 };
 
-console.log(person1.lastName);
+// console.log(person1); // ? Shows the same as after adding lastName as the latest version of object is always fetched
+
+// console.log(person1.lastName); // ? Outputs: undefined, in terminal as the lastname is not defined
+
+person1.lastName = "Thostrup";
+
+// console.log(person1);
+
+// console.log(person1.lastName); // ? Outputs: Thostrup. as it has been set
+
+// * Removing a property
+// 1st method // ! bad practice
+person1.lastName = undefined;
+
+// console.log(person1.lastName); // ? Outputs undefined
+// console.log(person1.middleName); // ? Outputs undefined
+
+// 2nd Method // ! Rarely done.
+delete person1.lastName;
+// console.log(person1.lastName); // ? Outputs undefined
+// console.log(person1); // ? Outputs object without the lastname property
+
+// * Let vs const
+let person2 = {
+  firstName: "Bella",
+  age: 8,
+  student: false,
+};
+
+person2.age++; // ? adds one year to age property
+
+// console.log(person2);
+
+// Const objects can still be changed and edited
+
+person1 = person2; // ? Cannot change objects but can change properties within objects

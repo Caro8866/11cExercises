c1eca4c5cf6cb0b02be748208ab267baadc2b3db
Caroline Thostrup <caro342h@stud.kea.dk>
2022-09-01 11:53:19 +0200

    not working mvc

--- mvc pattern/script.js
+++ mvc pattern/script.js
@@ -1,7 +1,8 @@
 "use strict";
 // model
 const model = ["Peter", "Paul", "Mary"];
-
+const h2Name = document.querySelector(".names");
+const inputName = document.querySelector("#name");
 // controller
 addNameToModel("Caroline");
 console.log(model);

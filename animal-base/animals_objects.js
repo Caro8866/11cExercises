4d30d7796790d391ded606d2aac6ed76a8cc8d98
Caroline Thostrup <caro342h@stud.kea.dk>
2022-09-05 10:11:25 +0200

    creating new objects with cleaned data

--- animal-base/animals_objects.js
+++ animal-base/animals_objects.js
@@ -2,44 +2,15 @@
 
 window.addEventListener("DOMContentLoaded", start);
 
-const allAnimals = [
-  {
-    name: "Mandu",
-    desc: "amazing",
-    type: "cat",
-    age: 10,
-  },
-  {
-    name: "Mia",
-    desc: "black",
-    type: "cat",
-    age: 10,
-  },
-  {
-    name: "Leeloo",
-    desc: "growing",
-    type: "dog",
-    age: 3,
-  },
-  {
-    name: "Toothless",
-    desc: "trained",
-    type: "dragon",
-    age: 14,
-  },
-  {
-    name: "ScoobyDoo",
-    desc: "wondering",
-    type: "dog",
-    age: 58,
-  },
-  {
-    name: "Horsey",
-    desc: "horsing",
-    type: "horse",
-    age: 10,
-  },
-];
+// prototype object
+const Animal = {
+  name: "-default name-",
+  desc: "-no description-",
+  type: "-unknown-",
+  age: 0,
+};
+
+const allAnimals = [];
 
 function start() {
   console.log("ready");
@@ -58,18 +29,31 @@ function loadJSON() {
 
 function prepareObjects(jsonData) {
   jsonData.forEach((jsonObject) => {
-    // TODO: Create new object with cleaned data - and store that in the allAnimals array
+    const animal = Object.create(Animal);
 
+    // extract data from json object
     const fullname = jsonObject.fullname;
 
+    // finding the spaces
     const firstSpace = fullname.indexOf(" ");
     const secondSpace = fullname.indexOf(" ", firstSpace + 1);
     const lastSpace = fullname.lastIndexOf(" ");
 
+    // Creating variables
     const name = fullname.substring(0, firstSpace);
     const desc = fullname.substring(secondSpace + 1, lastSpace);
     const type = fullname.substring(lastSpace + 1);
-    console.log(`name: _${name}_, desc: _${desc}_, type _${type}_`);
+
+    /* console.log(`name: _${name}_, desc: _${desc}_, type _${type}_`); */
+
+    // put cleaned data in newly created object
+    animal.name = name;
+    animal.desc = desc;
+    animal.type = type;
+    animal.age = jsonObject.age;
+
+    // add objects to the global array
+    allAnimals.push(animal);
   });
 
   displayList();
